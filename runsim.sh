acf273ba92960c3c9c55b86bfc5474d80c72cb1c
arjunsrivatsa <arjunsrivatsa11@gmail.com>
2022-03-20 04:13:46 -0400

    Delete runsim.sh

--- runsim.sh
+++
@@ -1,3 +0,0 @@
-srun -p pool1 --mem-per-cpu 46G -t 2-23:00:00 python3 /home/assrivat/MosaicSim/sim.py
-srun -p pool3-bigmem --mem-per-cpu 125G -t 3-23:00:00 python3 /home/assrivat/MosaicSim/sim.py
-srun -p rs2 --mem-per-cpu 100G -t 3-22:00:00 python3 /home/assrivat/MosaicSim/sim.py
