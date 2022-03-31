68afbcf053cafe9eba9ebcf69094b884a7b954c6
arjunsrivatsa <arjunsrivatsa11@gmail.com>
2022-03-30 20:36:02 -0400

    Delete fullrunrs2.py

--- fullrunrs2.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-
-inputid = sys.argv[1]
-#os.system("srun -p pool1 --mem-per-cpu 45G -t 3-00:00:00 python3 /home/assrivat/MosaicSim/sim.py; srun -p pool1 -c 7 --mem-per-cpu 2200 -t 3-00:00:00 python3 /home/assrivat/MosaicSim/pipeline/python_pipeline_v1.py {} 1 0 0 7 strelka None delly hg38_no_alt".format(randomid))
-#os.system("srun -p interactive --mem-per-cpu 10G -t 5:00:00 python3 /home/assrivat/MosaicSim/sim.py; srun -p interactive -c 4 --mem-per-cpu 2200 -t 5:00:00 python3 /home/assrivat/MosaicSim/pipeline/python_pipeline_v1.py {} 1 0 0 4 None None delly fakegenome; srun -p interactive --mem-per-cpu 10G python3 /home/assrivat/MosaicSim/fullAnalysis.py {}".format(randomid, randomid))
-os.system("srun -p rs2 --mem-per-cpu 150G -t 7-22:00:00 python3 /home/assrivat/MosaicSim/sim.py {}; srun -p rs2 -c 128 --mem-per-cpu 2200 -t 8-22:00:00 python3 /home/assrivat/MosaicSim/pipeline/python_pipeline_v1.py {} 1 0 0 128 strelka None delly hg38_no_alt; srun -p rs2 --mem-per-cpu 100G -t 8-22:00:00 python3 /home/assrivat/MosaicSim/fullAnalysis.py {}".format(inputid, inputid, inputid))
